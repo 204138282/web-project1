e90a647e426d1e42019d31c96fc2a66e6607a471
xiaolou <204138282@users.noreply.github.com>
2018-01-19 15:58:55 +0800

    Add files via upload
    web-test-project.

---
+++ test-site/scripts/main.js
@@ -0,0 +1,34 @@
+//h1
+var myHeading = document.querySelector("h1");
+myHeading.innerHTML = "Hello World";
+
+//img
+var myImage = document.querySelector("img");
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/test3.png") {
+    myImage.setAttribute("src", "images/test4.png");
+  } else {
+    myImage.setAttribute("src", "images/test3.png");
+  }
+};
+
+//button
+var myButton = document.querySelector("button");
+
+function setUserName() {
+  var myName = prompt("Please Enter Your Name");
+  localStorage.setItem("name", myName);
+  myHeading.innerHTML = "Mozilla is Cool," + myName;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.innerHTML = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+};
